cb1bdf308cf384e16673fdb33955d41af68bb12d
SamuelMidgley <midgley.sam@gmail.com>
2024-10-17 18:23:50 +0100

    Start of project

---
+++ server/build/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Server } from 'socket.io';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = Number(process.env.PORT) || 3500;
+const app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+const expressServer = app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+});
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+    socket.on('message', data => {
+        console.log(data);
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+    });
+});
